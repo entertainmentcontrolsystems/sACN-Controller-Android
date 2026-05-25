fda620dfc6f155bfd4e275a8a637fd3dfa9f6e3d
Mark Stuen <mark@ecslighting.com>
2026-05-25 00:37:44 -0700

    Add missing KeyboardOptions import to MainScreen.kt

--- app/src/main/java/com/sacn/controller/ui/MainScreen.kt
+++ app/src/main/java/com/sacn/controller/ui/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
